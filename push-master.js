c256525dcc807d46460e19831b2bb02a46bb8edd
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2020-10-14 20:03:14 +0530

    Update master branch

--- push-master.js
+++ push-master.js
@@ -1,5 +1,5 @@
 var execProcess = require('./exec_process.js');
-execProcess.result('sh push-update.sh', function (err, response) {
+execProcess.result('sh master-push.sh', function (err, response) {
   if (!err) {
     console.log(response);
   } else {

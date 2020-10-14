25a196114582ed940c410c9630ac6ec1c7b034a1
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2020-10-14 17:20:26 +0530

    Update gh-pages branch

--- update-master.js
+++ update-master.js
@@ -1,5 +1,5 @@
 var execProcess = require('./exec_process.js');
-execProcess.result('sh master.sh', function (err, response) {
+execProcess.result('sh master-update.sh', function (err, response) {
   if (!err) {
     console.log(response);
   } else {

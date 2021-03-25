2396f9889491f491a4731475f315628bce2fd3ea
Chaitanya Rayabharam <chaitanya.rsvnk@gmail.com>
2021-03-25 16:35:14 +0530

    Revert "Update gh-pages branch"
    This reverts commit f1d95b2d36648bb76b6bbe4f4cb69ae31df15ef2.

--- _site/push-gh-pages.js
+++
@@ -1,8 +0,0 @@
-var execProcess = require('./exec_process.js');
-execProcess.result('sh gh-pages-push.sh', function (err, response) {
-  if (!err) {
-    console.log(response);
-  } else {
-    console.log(err);
-  }
-});

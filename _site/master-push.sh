df23ce3c8b74cbe2ddd22988a9fc43b8e2b0bc43
Chaitanya Rayabharam <chaitanya.rsvnk@gmail.com>
2021-03-25 17:13:18 +0530

    Update gh-pages branch

---
+++ _site/master-push.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+echo ------------------------
+echo Git Current Status
+echo ------------------------
+git status
+echo ------------------------
+echo Pushing to master branch
+echo ------------------------
+git push -u origin master
+echo ------------------------
+echo Git Current Status
+echo ------------------------
+git status
+echo ------------------------
